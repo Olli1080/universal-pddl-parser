a40434a7481660f14232dc20b5d533805b2e4e1d
Olli1080 <50930405+Olli1080@users.noreply.github.com>
2025-05-07 16:44:01 +0200

    Initial rework

--- tests/DomainTests.cpp
+++ tests/DomainTests.cpp
@@ -1,13 +1,76 @@
-
+#include <filesystem>
 #include <fstream>
 #include <sstream>
 
-#include <minicppunit/MiniCppUnit.h>
+#include <gtest/gtest.h>
+
 #include <parser/Instance.h>
 
 using namespace parser::pddl;
 
-class DomainTests : public TestFixture< DomainTests >
+template < typename T >
+void checkEqual( T & prob, const std::string & file ) {
+	std::ifstream f(file.c_str());
+	if (!f) throw std::runtime_error(std::string("Failed to open file '") + file + "'");
+	std::string s, t;
+	
+	while(std::getline(f, s)){
+		t += s + "\n";
+	}
+
+	std::ostringstream ds;
+	ds << prob;
+	ASSERT_EQ( t, ds.str() );
+	std::ofstream of("ins.txt");
+	of<<ds.str();
+}
+
+TEST(DomainTests, LogisticsTest)
+{
+	std::cout << std::filesystem::current_path() << std::endl;
+
+	Domain dom( "domains/Log_dom.pddl" );
+	Instance ins( dom, "domains/Log_ins.pddl" );
+
+	checkEqual( dom, "expected/Log_dom.pddl" );
+	checkEqual( ins, "expected/Log_ins.pddl" );
+}
+
+TEST(DomainTests, ScheduleTest)
+{
+	Domain dom( "domains/Sched_dom.pddl" );
+	Instance ins( dom, "domains/Sched_ins.pddl" );
+
+	checkEqual( dom, "expected/Sched_dom.pddl" );
+	checkEqual( ins, "expected/Sched_ins.pddl" );
+}
+
+TEST(DomainTests, ElevatorTest)
+{
+	Domain dom( "domains/Elev_dom.pddl" );
+	Instance ins( dom, "domains/Elev_ins.pddl" );
+
+	checkEqual( dom, "expected/Elev_dom.pddl" );
+	checkEqual( ins, "expected/Elev_ins.pddl" );
+}
+
+TEST(DomainTests, TemporalTest)
+{
+	Domain dom( "domains/Mapana_dom.pddl" );
+	Instance ins( dom, "domains/Mapana_ins.pddl" );
+
+	checkEqual( dom, "expected/Mapana_dom.pddl" );
+	checkEqual( ins, "expected/Mapana_ins.pddl" );
+}
+
+int main(int argc, char **argv) 
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+/*
+class DomainTests : public testing::Test
 {
 public:
 	TEST_FIXTURE( DomainTests )
@@ -66,7 +129,9 @@ public:
 		checkEqual( dom, "tests/expected/Mapana_dom.pddl" );
 		checkEqual( ins, "tests/expected/Mapana_ins.pddl" );
 	}
-};
+};*/
 
-REGISTER_FIXTURE( DomainTests )
+//TEST(DomainTests, )
+
+//REGISTER_FIXTURE( DomainTests )
 
